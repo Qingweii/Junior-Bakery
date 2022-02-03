e2d3333c6e929fadeb09e3e0bdc21dba964ea89b
Qnwei <qingwei.wong5@gmail.com>
2022-02-03 22:07:06 +0800

    naming convention for CSS and HTML

--- script.js
+++ script.js
@@ -6,9 +6,15 @@ function showHome(){
     document.getElementById('about_us_page').style.display = 'block';
  };
 
+
+
 // Footer section (birthday submission)
 
 function bday(){
     alert('Your birthday is on its way, We\'ll be counting down to your special day ;)');
 };
+
+
+
+//
 
