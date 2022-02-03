74c7a8a94a804dd7a824ec37c4d5be2cd90a5244
Qnwei <qingwei.wong5@gmail.com>
2022-02-03 21:12:03 +0800

    message upon submission for birthday

--- script.js
+++ script.js
@@ -4,4 +4,11 @@ const duration = setTimeout(showHome,2000);
 function showHome(){
     document.getElementById('loading').style.display = 'none';
     document.getElementById('about_us_page').style.display = 'block';
- }
+ };
+
+// Footer section (birthday submission)
+
+function bday(event){
+    alert('Your birthday is on its way, We\'ll be counting down to your special day ;)');
+    event.preventDefault();
+};
