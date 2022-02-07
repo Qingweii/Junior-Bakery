a876922d92f5f4712c736ff43ba898f95f3826db
Qnwei <qingwei.wong5@gmail.com>
2022-02-07 16:40:30 +0800

    Adjustments for HTML ,CSS ,JS

--- script2.js
+++ script2.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
 
         for (var i = 0; i <response.length; i ++){
 
-            order = `${order}<tr id='${response[i]._id}'><td><img src = '${response[i].image}' width = '80' height = '100'></td><td>${response[i].name}</td><td>${response[i].price}</td><td>${response[i].quantity}</td></tr>`
+            order = `${order}<tr id='${response[i]._id}'><td><img src = '${response[i].image}' width = '100' height = '150'></td><td>${response[i].name}</td><td>${response[i].price}</td><td>${response[i].quantity}</td></tr>`
         }
 
         console.log(order);
@@ -31,3 +31,5 @@ $(document).ready(function(){
 
 
 });
+
+
